546b4d1cda8db21be14f4ac1d16a702f77be9b80
kobe24167 <765021003@qq.com>
2019-02-21 19:34:48 +0800

    add kafka demo

---
+++ kafkaOne/src/main/java/com/la/kafkaOne/SendController.java
@@ -0,0 +1,18 @@
+package com.la.kafkaOne;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class SendController {
+	@Autowired
+	private Producer producer;
+
+	@RequestMapping(value = "/send")
+	public String send() {
+	    producer.send();
+	    return "{\"code\":0}";
+	}
+}
