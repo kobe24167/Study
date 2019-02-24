a226a1903ca9218522d9b05ecddd446f4a09b2b0
kobe24167 <765021003@qq.com>
2019-02-24 10:42:59 +0800

    update

--- msApi/msApi/src/main/java/org/apache/dubbo/spring/boot/demo/consumer/DemoService.java
+++ msApi/msApi/src/main/java/org/apache/dubbo/spring/boot/demo/consumer/DemoService.java
@@ -14,29 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.dubbo.spring.boot.demo.provider.service;
-
-import org.apache.dubbo.spring.boot.demo.consumer.DemoService;
-import org.springframework.beans.factory.annotation.Value;
-
-import com.alibaba.dubbo.config.annotation.Service;
+package org.apache.dubbo.spring.boot.demo.consumer;
 
 /**
- * Default {@link DemoService}
+ * Demo Service interface
  *
- * @see DemoService
  * @since 2.7.0
  */
-@Service(version = "${demo.service.version}")
-public class DefaultDemoService implements DemoService {
+public interface DemoService {
 
-    /**
-     * The default value of ${dubbo.application.name} is ${spring.application.name}
-     */
-    @Value("${dubbo.application.name}")
-    private String serviceName;
+    String sayHello(String name);
 
-    public String sayHello(String name) {
-        return String.format("[%s] : Hello, %s", serviceName, name);
-    }
 }
