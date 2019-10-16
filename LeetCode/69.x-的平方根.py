aa4e17bfa8424ea0b96e0a75728ab735d5cac256
A9A7FD10AB8FD700CAD43011EEDFB4B7 <A9A7FD10AB8FD700CAD43011EEDFB4B7@i-search.com.cn>
2019-10-16 10:27:28 +0800

    update

---
+++ LeetCode/69.x-的平方根.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=69 lang=python3
+#
+# [69] x 的平方根
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(x**0.5)
+# @lc code=end
+
